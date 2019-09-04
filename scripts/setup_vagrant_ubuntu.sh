04617910c834d8f2a80aa96bc36bef4cbd22f7e2
hoangphuocbk <hoangphuocbk2.07@gmail.com>
2019-09-04 23:32:07 +0700

    update scripts ubuntu

--- scripts/setup_vagrant_ubuntu.sh
+++ scripts/setup_vagrant_ubuntu.sh
@@ -1,46 +1,33 @@
 #!/bin/bash -v
 
-apt-get update
-apt-get install -y wget git bridge-utils python python-pip tmux apt-transport-https software-properties-common
+sudo apt update
+sudo apt install -y wget git bridge-utils python python-pip tmux apt-transport-https software-properties-common
 
 # VirtualBox Installation
 # Add following line in "/etc/apt/sources.list"
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian `lsb_release -cs` contrib"
-sudo apt-get update
-sudo apt-get -y install virtualbox-5.2
+sudo apt -y install virtualbox
 
 ### Vagrant install
 # wget https://releases.hashicorp.com/vagrant/2.1.1/vagrant_2.1.1_x86_64.deb
-wget https://releases.hashicorp.com/vagrant/2.2.0/vagrant_2.2.0_x86_64.deb
-dpkg -i vagrant_2.2.0_x86_64.deb
+sudo apt -y install vagrant
 
 ## Ansible Install
-sudo apt-get update
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get -y install ansible
-ansible-galaxy install Juniper.junos
+sudo apt -y install ansible
 
 ## Install JunOS Ansible Module and Python Modules
 sudo ansible-galaxy install Juniper.junos
 
 pip install --upgrade pip
-sudo apt-get update
+sudo apt update
 pip install jxmlease
 pip install junos-eznc
 
 ## vQFX Box Addition
-cd /var/tmp
-wget http://10.84.5.120/cs-shared/images/vagrant-boxes/vqfx-re-virtualbox.box
-wget http://10.84.5.120/cs-shared/images/vagrant-boxes/vqfx10k-pfe-virtualbox.box
-
-vagrant box add --name juniper/vqfx10k-re /var/tmp/vqfx-re-virtualbox.box
-vagrant box add --name juniper/vqfx10k-pfe /var/tmp/vqfx10k-pfe-virtualbox.box
+vagrant box add juniper/vqfx10k-re
+vagrant box add juniper/vqfx10k-pfe
 
 # Download and Addd CentOS-7.5 Box
-vagrant box add qarham/CentOS7.5-350GB
+vagrant box add centos/7
 
 
 echo "List Box"
